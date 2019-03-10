17dc6c2855b6ca45a283961f7da8a5c22b0d6829
Steve Young <steveyng4582@comcast.net>
2019-03-10 16:56:12 -0400

    Added daily high and low temperatures to displayed output

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,9 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' +body.currently.apparentTemperature+ ' degrees out. There is a '+ body.currently.precipProbability +' chance of rain.');  
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' +body.currently.apparentTemperature+ ' degrees out with a high of '+ 
+            body.daily.data[0].apparentTemperatureHigh +' and a low of '+ body.daily.data[0].apparentTemperatureLow +'. There is a '+ 
+            body.currently.precipProbability +' chance of rain.');  
         }
     });
 };
